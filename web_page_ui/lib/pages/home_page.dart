619c1f7019fdfb24111f77fb2411e9e26b1738f4
serhankars <serhankars@hotmail.com>
2022-08-30 21:37:59 -0400

    Added Gesture detector to home page

--- web_page_ui/lib/pages/home_page.dart
+++ web_page_ui/lib/pages/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -36,145 +34,145 @@ class _HomePageState extends State<HomePage> {
     super.setState(fn);
   }
 
+  void _onPositionChange(Offset position, BoxConstraints constraints) {
+    _mouseX = position.dx;
+    _mouseY = position.dy;
+    double leftBorder = constraints.maxWidth / 2 - 100;
+    double rightBorder = constraints.maxWidth / 2 + 100;
+
+    setState(() {
+      if (_mouseX < leftBorder) {
+        _mousePositionHorizontal = "_left";
+      } else if (_mouseX > rightBorder) {
+        _mousePositionHorizontal = "_right";
+      } else {
+        _mousePositionHorizontal = "";
+      }
+
+      if (_mouseY < topBorder) {
+        _mousePositionVertical = "_up";
+      } else if (_mouseY > bottomBorder) {
+        _mousePositionVertical = "_down";
+      } else {
+        _mousePositionVertical = "";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constraints) {
-        return MouseRegion(
-          child: Container(
-            constraints: const BoxConstraints.expand(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const FittedBox(
-                  alignment: Alignment.center,
-                  fit: BoxFit.scaleDown,
-                  child: Text(
-                    "Serhan Kars",
-                    style: TextStyle(
-                        fontFamily: "BitmapFont",
-                        fontWeight: FontWeight.w700,
-                        fontSize: 50),
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  width: double.infinity,
-                  height: 270,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Stack(
-                          children: [
-                            const CircleAvatar(
-                              radius: 100,
-                              backgroundImage:
-                                  AssetImage("assets/imgs/profile.png"),
-                            ),
-                            CircleAvatar(
-                              radius: 100,
-                              backgroundImage: AssetImage(
-                                  "assets/imgs/profile${_mousePositionHorizontal}${_mousePositionVertical}.png"),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        const Text(
-                          "Software Engineer",
-                          style:
-                              TextStyle(fontFamily: "BitmapFont", fontSize: 20),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: Image.asset('assets/imgs/linkedin.png'),
-                      iconSize: 45,
-                      onPressed: () async {
-                        final Uri url = Uri.parse(
-                            'https://www.linkedin.com/in/serhan-kars-0ba5b238/');
-
-                        await launchUrl(url);
-                      },
-                    ),
-                    IconButton(
-                      icon: Image.asset('assets/imgs/github.png'),
-                      iconSize: 30,
-                      onPressed: () async {
-                        final Uri url =
-                            Uri.parse('https://github.com/serhankars');
-
-                        await launchUrl(url);
-                      },
-                    ),
-                    IconButton(
-                      icon: Image.asset('assets/imgs/twitter.png'),
-                      iconSize: 30,
-                      onPressed: () async {
-                        final Uri url =
-                            Uri.parse('https://twitter.com/learningathing');
-
-                        await launchUrl(url);
-                      },
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    // IconButton(
-                    //   icon: Image.asset('assets/imgs/twitter.png'),
-                    //   iconSize: 30,
-                    //   onPressed: () async {
-                    //     final Uri url =
-                    //         Uri.parse('https://twitter.com/learningathing');
-
-                    //     await launchUrl(url);
-                    //   },
-                    // ),
-                  ],
-                )
-              ],
-            ),
-          ),
-          onHover: (event) {
-            _mouseX = event.position.dx;
-            _mouseY = event.position.dy;
-            double leftBorder = constraints.maxWidth / 2 - 100;
-            double rightBorder = constraints.maxWidth / 2 + 100;
-
+        return GestureDetector(
+          onPanUpdate: (details) {
+            _onPositionChange(details.globalPosition, constraints);
+          },
+          onPanEnd: (DragEndDetails details) {
             setState(() {
-              if (_mouseX < leftBorder) {
-                _mousePositionHorizontal = "_left";
-              } else if (_mouseX > rightBorder) {
-                _mousePositionHorizontal = "_right";
-              } else {
-                _mousePositionHorizontal = "";
-              }
-
-              if (_mouseY < topBorder) {
-                _mousePositionVertical = "_up";
-              } else if (_mouseY > bottomBorder) {
-                _mousePositionVertical = "_down";
-              } else {
-                _mousePositionVertical = "";
-              }
+              _mousePositionHorizontal = "";
+              _mousePositionVertical = "";
             });
           },
+          child: MouseRegion(
+            child: Container(
+              constraints: const BoxConstraints.expand(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const FittedBox(
+                    alignment: Alignment.center,
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      "Serhan Kars",
+                      style: TextStyle(
+                          fontFamily: "BitmapFont",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 50),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    height: 270,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Stack(
+                            children: [
+                              const CircleAvatar(
+                                radius: 100,
+                                backgroundImage:
+                                    AssetImage("assets/imgs/profile.png"),
+                              ),
+                              CircleAvatar(
+                                radius: 100,
+                                backgroundImage: AssetImage(
+                                    "assets/imgs/profile${_mousePositionHorizontal}${_mousePositionVertical}.png"),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text(
+                            "Software Engineer",
+                            style: TextStyle(
+                                fontFamily: "BitmapFont", fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Image.asset('assets/imgs/linkedin.png'),
+                        iconSize: 45,
+                        onPressed: () async {
+                          final Uri url = Uri.parse(
+                              'https://www.linkedin.com/in/serhan-kars-0ba5b238/');
+
+                          await launchUrl(url);
+                        },
+                      ),
+                      IconButton(
+                        icon: Image.asset('assets/imgs/github.png'),
+                        iconSize: 30,
+                        onPressed: () async {
+                          final Uri url =
+                              Uri.parse('https://github.com/serhankars');
+
+                          await launchUrl(url);
+                        },
+                      ),
+                      IconButton(
+                        icon: Image.asset('assets/imgs/twitter.png'),
+                        iconSize: 30,
+                        onPressed: () async {
+                          final Uri url =
+                              Uri.parse('https://twitter.com/learningathing');
+
+                          await launchUrl(url);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            onHover: (event) {
+              _onPositionChange(event.position, constraints);
+            },
+          ),
         );
       }),
     );
